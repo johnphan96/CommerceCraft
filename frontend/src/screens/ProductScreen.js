44abb3b8c79e3688d4303e23d94f1f562a9b1336
John Phan <johnphan010@gmail.com>
2021-01-10 19:41:14 -0800

    Implemented new API for product details in server. Implemented Redux to ProductScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,54 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Rating from "../components/Rating";
-import data from "../data";
 import { Link } from "react-router-dom";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { detailsProduct } from "../actions/productActions";
 
 function ProductScreen(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
-  if (!product) {
-    return <div>Product Not Found</div>;
-  }
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
   return (
     <div>
-      <Link to="/">Back to result</Link>
-      <div className="row top">
-        <div className="col-2">
-          <img className="large" src={product.image} alt={product.name}></img>
-        </div>
-        <div className="col-1">
-          <ul>
-            <li>
-              <h1>{product.name}</h1>
-            </li>
-            <li>
-              <Rating
-                rating={product.rating}
-                numReviews={product.numReviews}
-              ></Rating>
-            </li>
-            <li>
-              <div className="price">
-                Price:{" "}
-                <img
-                  className="price-image"
-                  src="/images/emerald.png"
-                  alt="price"
-                />
-                <span>{product.price}</span>
-              </div>
-            </li>
-            <li>
-              Description: <p>{product.description}</p>
-            </li>
-          </ul>
-        </div>
-        <div className="col-1">
-          <div className="card card-body">
-            <ul>
-              <li>
-                <div className="row">
-                  <div>Price</div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div>
+          <Link to="/">Back to result</Link>
+          <div className="row top">
+            <div className="col-2">
+              <img
+                className="large"
+                src={product.image}
+                alt={product.name}
+              ></img>
+            </div>
+            <div className="col-1">
+              <ul>
+                <li>
+                  <h1>{product.name}</h1>
+                </li>
+                <li>
+                  <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                  ></Rating>
+                </li>
+                <li>
                   <div className="price">
+                    Price:{" "}
                     <img
                       className="price-image"
                       src="/images/emerald.png"
@@ -56,25 +54,47 @@ function ProductScreen(props) {
                     />
                     <span>{product.price}</span>
                   </div>
-                </div>
-                <div className="row">
-                  <div>Status</div>
-                  <div>
-                    {product.countInStock > 0 ? (
-                      <span className="success">In Stock</span>
-                    ) : (
-                      <span className="danger">Unavailable</span>
-                    )}
-                  </div>
-                </div>
-              </li>
-              <li>
-                <button className="primary block">Add to Cart</button>
-              </li>
-            </ul>
+                </li>
+                <li>
+                  Description: <p>{product.description}</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-1">
+              <div className="card card-body">
+                <ul>
+                  <li>
+                    <div className="row">
+                      <div>Price</div>
+                      <div className="price">
+                        <img
+                          className="price-image"
+                          src="/images/emerald.png"
+                          alt="price"
+                        />
+                        <span>{product.price}</span>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div>Status</div>
+                      <div>
+                        {product.countInStock > 0 ? (
+                          <span className="success">In Stock</span>
+                        ) : (
+                          <span className="danger">Unavailable</span>
+                        )}
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <button className="primary block">Add to Cart</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
