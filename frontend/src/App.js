5b2df192cfd5bfadcedad2eb98d6df083ac511d8
John Phan <johnphan010@gmail.com>
2021-01-10 21:36:58 -0800

    Implemented hook in ProductScreen to get qty to add to cart. Implemented CartScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">CommerceCraft</a>
+            <a className="brand" href="/">
+              CommerceCraft
+            </a>
           </div>
           <div>
             <a href="/cart">Cart</a>
@@ -17,6 +20,7 @@ function App() {
           </div>
         </header>
         <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
